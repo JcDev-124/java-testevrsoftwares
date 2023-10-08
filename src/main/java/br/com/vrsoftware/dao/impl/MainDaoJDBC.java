404c3a7dde02976366e754a5347ddf60c7ec8e5e
julio <julio.prog124@gmail.com>
2023-10-08 17:24:04 -0300

    solucionado em partes o problema da automatizacao

--- src/main/java/br/com/vrsoftware/dao/impl/MainDaoJDBC.java
+++ src/main/java/br/com/vrsoftware/dao/impl/MainDaoJDBC.java
@@ -33,11 +33,10 @@ public class MainDaoJDBC implements MainDao {
         PreparedStatement st = null;
 
         try {
-            st = conn.prepareStatement(
-                    "CREATE DATABASE IF NOT EXISTS sistemapdv;"
-            );
-
-            // Execute a query to create the database structure
+            st = conn.prepareStatement("CREATE DATABASE sistemapdv"
+                    + " WITH OWNER = postgres"
+                    + " ENCODING = 'WIN1252'" // 
+                    + " TEMPLATE = template0;");
             st.execute();
         } catch (SQLException e) {
             throw new DbException("Erro ao criar a estrutura do banco de dados: " + e.getMessage());
@@ -67,7 +66,8 @@ public class MainDaoJDBC implements MainDao {
                     + "    id SERIAL PRIMARY KEY,"
                     + "    data TIMESTAMPTZ,"
                     + "    cliente_id INTEGER REFERENCES clientes(id),"
-                    + "    status INTEGER"
+                    + "    status VARCHAR(10),"
+                    + "    valorTotal DOUBLE PRECISION"
                     + ");"
                     + "CREATE TABLE IF NOT EXISTS OrdemDeVenda ("
                     + "    id_ordem_de_venda SERIAL PRIMARY KEY,"
@@ -93,26 +93,26 @@ public class MainDaoJDBC implements MainDao {
         PreparedStatement st = null;
 
         try {
+
             st = conn.prepareStatement(
-                    "CREATE OR REPLACE FUNCTION IF NOT EXISTS insere_id_venda_na_ordem() "
-                    + "RETURNS TRIGGER AS $$"
-                    + "BEGIN"
-                    + "    UPDATE OrdemDeVenda"
-                    + "    SET id_venda = NEW.id"
-                    + "    WHERE id_venda IS NULL;"
-                    + "    RETURN NEW;"
-                    + "END;"
-                    + "$$ LANGUAGE plpgsql;"
-                    + "CREATE TRIGGER IF NOT EXISTS atualiza_id_venda_trigger "
+                    "CREATE OR REPLACE FUNCTION insere_id_venda_na_ordem() "
+                    + "RETURNS TRIGGER AS $$ "
+                    + "BEGIN "
+                    + "    UPDATE OrdemDeVenda "
+                    + "    SET id_venda = NEW.id "
+                    + "    WHERE id_venda IS NULL; "
+                    + "    RETURN NEW; "
+                    + "END; "
+                    + "$$ LANGUAGE plpgsql; "
+                    + "CREATE TRIGGER atualiza_id_venda_trigger "
                     + "AFTER INSERT ON vendas "
                     + "FOR EACH ROW "
                     + "EXECUTE FUNCTION insere_id_venda_na_ordem();"
             );
-
             // Execute a query to create the database structure
             st.execute();
         } catch (SQLException e) {
-            throw new DbException("Erro ao criar a estrutura do banco de dados: " + e.getMessage());
+            throw new DbException("Erro ao criar o trigger no banco de dados: " + e.getMessage());
         } finally {
             // Close the PreparedStatement
             DB.CloseStatement(st);
